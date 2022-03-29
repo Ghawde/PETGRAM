64a558be395840a43f2edb4d11e0194b9fa4a459
prachii2002 <88281952+prachii2002@users.noreply.github.com>
2022-03-30 00:31:38 +0530

    home page/landing page

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+<!--    <link href="images/logo.jpg" rel="shortcut icon"> -->
+    <title>PETGRAM</title>
+  
+  <!-- core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">  
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	<link rel="icon" href="logo69.jpeg" type="image/x-icon">
+</head>
+
+<!--*********************************************START OF NAVIGATION BAR****************************************--> 
+<body>
+          
+      <nav class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                     <a href="index.php"><h4 class="wow fadeInDown" style="margin-top:20px; color:#FFF;"> 
+                <!--      <img src="images/logo.jpg"  width="15% "/> --> PETGRAM</h4></a>
+                </div>
+				   <div class="collapse navbar-collapse navbar-right wow fadeInDown">
+                    <ul class="nav navbar-nav">
+                         <li class="active"><a href="index.php"><i class="fa fa-home"></i>Home</a></li>
+                        <li ><a href="about-us.php">About Us</a></li>
+                        <li ><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                                                               
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->
+		
+<!--*********************************************START SLIDER************************************************-->
+
+<div class="container-fluid">
+    <br>
+        <div class="col-md-9 wow fadeInDown">
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="background-color:#000; padding:10px;">
+              <!-- Indicators -->
+              <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+              </ol>
+            
+              <!-- Wrapper for slides -->
+              <div class="">
+
+                <div class="">
+                  <img src="images/logo.png" alt="" class="">
+                </div>
+                
+               
+              </div>
+			  <!-- Controls -->
+              <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+              </a>
+              <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+              </a>
+            </div>
+			 </div>
+
+        <div class="col-md-3" >
+            <div class="panel panel-default wow fadeInDown">
+              <!-- Default panel contents -->
+            
+              <div class="panel-heading wow fadeInDown" style="font-weight:bold; font-size:16px; color:#36648B;">Welcome To Our Website <i class="glyphicon glyphicon-calendar"></i> <?php echo date('M d, Y');?></div>
+            
+            </div>
+			<div class="panel panel-default wow fadeInDown">
+              <!-- Default panel contents -->
+              <div class="panel-heading wow fadeInDown" style="font-weight:bold; font-size:16px; color:#36648B;">Pets </div>
+              <ul class="list-group">
+               
+                <li class="list-group-item wow fadeInDown">Dog  <span style="color:#8B8B00; font-weight:bold;"> </span> <span style="color:#EE5C42;" class="glyphicon glyphicon-ok pull-right"></span></li>
+               
+                  
+                   <li class="list-group-item wow fadeInDown">Cat <span style="color:#8B8B00; font-weight:bold;"> </span> <span style="color:#EE5C42;" class="glyphicon glyphicon-ok pull-right"></span></li>
+                
+
+            
+              </ul>
+              
+            </div>
+		
+<!-- footer -->		
+		<footer id="footer" class="midnight-blue wow fadeInDown">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 wow fadeInDown">
+              <a target="_blank" href="#" title="#">Let's be a home for happy soul and adopt the needy one!</a>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right wow fadeInDown">
+                        <li class="wow fadeInDown"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                        
+                       
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+</body>
+</html>
+ 
