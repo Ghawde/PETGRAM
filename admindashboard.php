10e78b960b802ff8960a10d19c2ca2caabbfba6d
stuborn-akshii <shatushatakshi@gmail.com>
2022-03-31 15:40:11 +0530

    Admin Dashboard

--- admindashboard.php
+++ admindashboard.php
@@ -48,25 +48,25 @@
           <li class="nav-item ">
             <a class="nav-link" href="./tables.html">
               <i class="material-icons">content_paste</i>
-              <p>Table List</p>
+              <p>Pets Available</p>
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link" href="./typography.html">
               <i class="material-icons">library_books</i>
-              <p>Typography</p>
+              <p>Pet Information</p>
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link" href="./icons.html">
               <i class="material-icons">bubble_chart</i>
-              <p>Icons</p>
+              <p>Pet Requests</p>
             </a>
           </li>
           <li class="nav-item ">
             <a class="nav-link" href="./map.html">
               <i class="material-icons">location_ons</i>
-              <p>Maps</p>
+              <p>Pet Status</p>
             </a>
           </li>
           <li class="nav-item ">
